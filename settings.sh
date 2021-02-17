fe28d405e3413b89eb8d639acf1e6ade2652d03e
pssilva22 <pedro442.rodrigues@gmail.com>
2021-02-16 23:53:21 -0300

    arquivos inicializacao do server

---
+++ settings.sh
@@ -0,0 +1,9 @@
+# Interface de rede utilizado pelo servidor, dhcp cliente
+LAN_IF = ""
+
+# Lista de pacotes extras a serem instalados no servidor 
+PACKAGES = "nmap"
+
+# Configuracao Vagrantbox  
+VB_IMAGE = ubuntu/bionic64
+VB_RAM = 1024
